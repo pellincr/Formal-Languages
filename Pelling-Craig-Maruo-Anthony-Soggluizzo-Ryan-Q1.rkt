b8ef77909e5ce0ad8b6974e4204ad9b9d89c2cf4
pellincr <44884108+pellincr@users.noreply.github.com>
2020-01-29 19:08:25 -0500

    Remove Nonreachable States From a Machine

---
+++ Pelling-Craig-Maruo-Anthony-Soggluizzo-Ryan-Q1.rkt
@@ -0,0 +1,80 @@
+#lang racket
+(require test-engine/racket-tests)
+(require fsm)
+
+(define TESTONE
+  (make-dfa '(Q0 Q1 Q3)
+            '(a b)
+            'Q0
+            '(Q0 Q1)
+            '((Q0 a Q1)
+              (Q1 a Q0)
+              (Q3 a Q3)
+              (Q0 b Q0)
+              (Q1 b Q1)
+              (Q3 b Q3))))
+
+(define TESTONE-EQ
+  (make-dfa '(Q0 Q1)
+            '(a b)
+            'Q0
+            '(Q0 Q1)
+            '((Q0 a Q1)
+              (Q1 a Q0)
+              (Q0 b Q0)
+              (Q1 b Q1))))
+
+
+
+
+;remove-nonreachable: dfa -> dfa
+;Purpose: to remove the non-reachable states from the given dfa
+(define (remove-nonreachable dfa)
+  ;INVENTORY
+  ;(sm-getstates dfa) -> returns the states of the machine
+  ;(sm-getalphabet dfa) -> retruns the alphabet of the given state
+  ;(sm-getrules dfa) -> returns the rules of the given dfa
+  ;(sm-getstart dfa) -> retruns the starting state
+  ;(sm-getfinals dfa) -> returns the list of final states
+  (make-dfa
+   (filter reachablestate? dfa)
+   (sm-getalphabet dfa)
+   (sm-getstart dfa)
+   (sm-getfinals dfa)
+   (filter reachablerule? dfa)))
+
+
+;reachablestate?: dfa -> boolean
+;Purpose: to determine if the given state is reachable
+(define (reachablestate? dfa)
+  ;INVENTORY
+  ;(sm-getstates dfa) -> returns the states of the machine
+  ;(sm-getalphabet dfa) -> retruns the alphabet of the given state
+  ;(sm-getrules dfa) -> returns the rules of the given dfa
+  ;(sm-getstart dfa) -> retruns the starting state
+  ;(sm-getfinals dfa) -> returns the list of final states
+  )
+
+;reachablestate?: dfa -> boolean
+(define (reachablestate? dfa)
+  ;INVENTORY
+  ;(sm-getstates dfa) -> returns the states of the machine
+  ;(sm-getalphabet dfa) -> retruns the alphabet of the given state
+  ;(sm-getrules dfa) -> returns the rules of the given dfa
+  ;(sm-getstart dfa) -> retruns the starting state
+  ;(sm-getfinals dfa) -> returns the list of final states
+  )
+
+
+
+(check-expect (sm-testequiv? (remove-nonreachable TESTONE) TESTONE-EQ) #t)
+
+
+
+
+
+
+
+
+
+(test)
