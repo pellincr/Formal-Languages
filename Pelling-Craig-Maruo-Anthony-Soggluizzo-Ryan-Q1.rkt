a1653257fe5bb185252aa620254c0bf7558df9ac
AMauro26 <43528019+AMauro26@users.noreply.github.com>
2020-02-03 19:28:14 -0500

    Update Pelling-Craig-Maruo-Anthony-Soggluizzo-Ryan-Q1.rkt

--- Pelling-Craig-Maruo-Anthony-Soggluizzo-Ryan-Q1.rkt
+++ Pelling-Craig-Maruo-Anthony-Soggluizzo-Ryan-Q1.rkt
@@ -24,7 +24,91 @@
               (Q0 b Q0)
               (Q1 b Q1))))
 
+(define TESTTWO
+  (make-dfa '(Q0 Q1 Q2 Q3 Q4 Q5)
+            '(a b)
+            'Q1
+            '(Q0 Q4 Q5)
+            '((Q0 a Q5)
+              (Q1 a Q2)
+              (Q2 a Q3)
+              (Q3 a Q4)
+              (Q4 a Q4)
+              (Q5 a Q0)
+              (Q0 b Q5)
+              (Q1 b Q3)
+              (Q3 b Q2)
+              (Q2 b Q4)
+              (Q4 b Q4)
+              (Q5 b Q0)
+              )))
 
+
+(define TESTTWO-EQ
+  (make-dfa '(Q1 Q2 Q3 Q4)
+            '(a b)
+            'Q1
+            '(Q4)
+            '((Q1 a Q2)
+              (Q2 a Q3)
+              (Q3 a Q4)
+              (Q4 a Q4)
+              (Q1 b Q3)
+              (Q3 b Q2)
+              (Q2 b Q4)
+              (Q4 b Q4))))
+
+(define TESTTHREE
+  (make-dfa '(Q0 Q1 Q2 Q3 Q4 Q5)
+            '(a b)
+            'Q0
+            '(Q5)
+            '((Q0 a Q1)
+              (Q1 a Q3)
+              (Q3 a Q5)
+              (Q5 a Q5)
+              (Q2 a Q1)
+              (Q4 a Q3)
+              (Q0 b Q3)
+              (Q3 b Q1)
+              (Q1 b Q5)
+              (Q5 b Q5)
+              (Q2 b Q2)
+              (Q4 b Q4))))
+
+(define TESTTHREE-EQ
+  (make-dfa '(Q0 Q1 Q3 Q5)
+            '(a b)
+            'Q0
+            '(Q5)
+            '((Q0 a Q1)
+              (Q1 a Q3)
+              (Q3 a Q5)
+              (Q5 a Q5)
+              (Q0 b Q3)
+              (Q3 b Q1)
+              (Q1 b Q5)
+              (Q5 b Q5))))
+
+
+
+; Proof by induction
+;
+; KEY: U == nonreachable state
+;
+; Say A and B are dfas
+; Prove: (remove-nonreachable (dfa B)) == (dfaA)
+;                            Where (dfa B) = {dfa A, (N)U} && (dfa A) has no nonreachale states
+;
+; Base Case: N = 0, therefore, dfa B contains no nonreachable states, therefore (dfa B) == (dfa A), therefore (remove-nonreachable (dfa B)) == (dfa A)
+;
+; Assume: For some K where K = N, (remove-nonreachable (dfa B) == (dfa A))
+;                            Where (dfa B) = {dfa A, (K)U} && (dfa A) has no nonreachale states
+;
+; Prove: (remove-nonreachable (dfa B)) == (dfa A)
+;                            Where (dfa B) = {dfa A, (K+1)U} && (dfa A) has no nonreachale states
+;
+; dfa B = {dfa A, (K+1)U} = {{dfa A, (K)U}, (1)U} = {dfa A,(1)U}
 
 
 ;remove-nonreachable: dfa -> dfa
@@ -86,6 +170,8 @@
 
 
 (sm-testequiv? TESTONE TESTONE-EQ)
+(sm-testequiv? TESTTWO TESTTWO-EQ)
+(sm-testequiv? TESTTHREE TESTTHREE-EQ)
 
 
 
